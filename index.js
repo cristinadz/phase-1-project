a7ccbf78077558003f5080e0969da8ff0f9782d2
+ <diazcristina.la@gmail.com>
2022-05-09 10:39:18 -0700

    added renderDetails function

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    fetch('https://ghibliapi.herokuapp.com/films')
-        .then(resp => resp.json())
-        .then (filmsArray => {
-            renderSide(filmsArray)
-        })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://ghibliapi.herokuapp.com/films")
+    .then((resp) => resp.json())
+    .then((filmsArray) => {
+      renderSide(filmsArray);
+    });
 
-const renderSide = filmsArray => filmsArray.forEach(titleLoop)
+  const renderSide = (filmsArray) => filmsArray.forEach(titleLoop);
 
-const collection = document.querySelector('#collection')
-    console.log(collection)
+  const collection = document.getElementById("collection");
+  const detailsContainer = document.getElementById("film-details");
 
-const titleLoop = (filmObj) => {
-    const sideList = document.createElement('li')
-    sideList.textContent = filmObj.title
-    collection.append(sideList)
-}
-       
+  const titleLoop = (filmObj) => {
+    const sideList = document.createElement("ul");
+    sideList.textContent = filmObj.title;
+    collection.append(sideList);
+    sideList.addEventListener("click", () => renderDetails(filmObj));
+  };
+
+  const renderDetails = (filmObj) => {
+    detailsContainer.innerHTML = "";
+    const img = document.createElement("img");
+    const title = document.createElement("h2");
+    const description = document.createElement("p");
+
+    img.src = filmObj.image;
+    title.textContent = filmObj.title;
+    description.textContent = filmObj.description;
+    detailsContainer.append(img, title, description);
+  };
+});
