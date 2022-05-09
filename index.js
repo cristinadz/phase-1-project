2173ca48c930ed487e3c3ba5eb9ae78a1e3ff7cf
Daniel <danchan020@gmail.com>
2022-05-09 09:58:59 -0700

    renderSide func

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     fetch('https://ghibliapi.herokuapp.com/films')
-        .then(resp => resp.json)
+        .then(resp => resp.json())
         .then (filmsArray => {
-            
+            renderSide(filmsArray)
         })
 })
+
+const renderSide = filmsArray => filmsArray.forEach(titleLoop)
+
+const collection = document.querySelector('#collection')
+    console.log(collection)
+
+const titleLoop = (filmObj) => {
+    const sideList = document.createElement('li')
+    sideList.textContent = filmObj.title
+    collection.append(sideList)
+}
+       
